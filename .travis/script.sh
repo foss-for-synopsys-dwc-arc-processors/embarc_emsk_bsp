c1c8066aa2475bb64ca392121704ed7a43e81078
Jingru Wang <jingru@synopsys.com>
2018-07-16 14:34:45 +0800

    travis: add travis ci app to this repo
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -13,7 +13,7 @@ set -x
     cd board/emsk/configs/23 || die
     tcftool arcem7d.tcf -q -x C_defines,arc_core_config.h -x gcc.arg || die
     mv arc_core_config.h gcc.arg ../../../../example/hello/arcgnu || die
-    cp ld/linker_gnu.ld ../../../../../example/hello/arcgnu || die
+    cp ld/linker_gnu.ld ../../../../example/hello/arcgnu || die
 
     cd ../../../../.travis || die
     [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 11 -o $CUR_CORE != arcem6 ] || {
