8bd94301ac46119fdf3bd1d51ac365546708aa85
Jingru Wang <jingru@synopsys.com>
2018-07-16 14:29:02 +0800

    travis: add travis ci app to this repo
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

---
+++ .travis/build.py
@@ -0,0 +1,60 @@
+import json
+import os
+import sys
+
+folder = ".travis"
+make_path = dict()
+makefile = 'makefile'
+
+def get_makefile(path): # to find the makefile in the embarc_applications
+	path = os.path.abspath(path)
+	for root, dirs, files in os.walk(path):
+		dirs[:] = [d for d in dirs  if not d.startswith('.')]
+		for f in files:
+			if f == makefile:
+				key = os.path.basename(root)+'_'+os.path.basename(os.path.dirname(root))
+				make_path[key] = root
+
+def embarc_makefile(paths): # to confirm the file is a embarc makefile
+	for (k,v) in paths.items():
+		with open(os.path.join(v,"makefile")) as f:
+			embarc_root = False
+			appl = False
+			lines = f.read().splitlines()
+			for line in lines:
+				if "EMBARC_ROOT" in line:
+					embarc_root = True
+				if "APPL" in line:
+					appl = True
+			if not (embarc_root and appl):
+				paths.pop(k)
+
+if __name__ == '__main__':
+	result = {}
+	cwd_path = os.getcwd() # /.travis
+	bsp_path = os.path.dirname(cwd_path) # embarc_emsk_bsp
+	bsp_application = bsp_path + "/example"
+
+	get_makefile(bsp_application)
+	embarc_makefile(make_path)
+	for (k,v) in make_path.items():
+		result[k] = 0
+		print("application[%s]=" %k,v)
+		pathin = v.replace("\\", "/")
+		os.chdir(pathin)
+		print("make configuration: ",sys.argv[1])
+		print("start compile")
+		os.system("make "+sys.argv[1]+" clean") 
+
+		if os.system("make "+sys.argv[1]+" -k") != 0:
+
+			result[k] = 1
+		pathout = cwd_path 
+		os.chdir(pathout)
+	print("Compilation result")
+	print(result)
+	for (k,v) in result.items():
+		if v == 1:
+			print("build failed")
+			sys.exit(1)
+	sys.exit(0)
---
+++ .travis/script.sh
@@ -0,0 +1,41 @@
+die() {
+    echo " *** ERROR: " $*
+    exit 1
+
+}
+set -x
+
+[ $TRAVIS_OS_NAME != linux ] || {
+
+    export PATH=/tmp/arc_gnu_2017.09_prebuilt_elf32_le_linux_install/bin:$PATH || die
+    git checkout -- . || die
+
+    cd board/emsk/configs/23 || die
+    tcftool arcem7d.tcf -q -x C_defines,arc_core_config.h -x gcc.arg || die
+    mv arc_core_config.h gcc.arg ../../../../example/hello/arcgnu || die
+    cp ld/linker_gnu.ld ../../../../../example/hello/arcgnu || die
+
+    cd ../../../../.travis || die
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 11 -o $CUR_CORE != arcem6 ] || {
+
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+    }
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 22 -o $CUR_CORE != arcem7d ] || {
+
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+    }
+
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 22 -o $CUR_CORE != arcem11d ] || {
+
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+    }
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 23 -o $CUR_CORE != arcem7d ] || {
+
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+    }
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 23 -o $CUR_CORE != arcem11d ] || {
+
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+    }
+
+}
