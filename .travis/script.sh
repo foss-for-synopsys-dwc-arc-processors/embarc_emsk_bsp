ba1bfb2fc25eb32afb1792dae1de2e498d4b6be6
Jingru Wang <jingru@synopsys.com>
2018-07-16 21:18:52 +0800

    travis: modify build.py for travis ci build
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .travis/script.sh
+++ .travis/script.sh
@@ -10,26 +10,19 @@ set -x
     export PATH=/tmp/arc_gnu_2017.09_prebuilt_elf32_le_linux_install/bin:$PATH || die
     git checkout -- . || die
     cd .travis || die
-    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 11 -o $CUR_CORE != arcem6 ] || {
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 11 ] || {
 
-        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER}" || die
     }
-    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 22 -o $CUR_CORE != arcem7d ] || {
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 22 ] || {
 
-        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER}" || die
     }
 
-    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 22 -o $CUR_CORE != arcem11d ] || {
+    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 23 ] || {
 
-        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
+        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER}" || die
     }
-    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 23 -o $CUR_CORE != arcem7d ] || {
 
-        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
-    }
-    [ $TOOLCHAIN != gnu -o $BOARD != emsk -o $BD_VER != 23 -o $CUR_CORE != arcem11d ] || {
-
-        python3 build.py "TOOLCHAIN=${TOOLCHAIN} BOARD=${BOARD} BD_VER=${BD_VER} CUR_CORE=${CUR_CORE}" || die
-    }
 
 }
