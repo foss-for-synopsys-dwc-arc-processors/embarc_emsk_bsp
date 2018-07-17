03b00d27c571bab4611e0cea03158e4ee90259e6
Jingru Wang <jingru@synopsys.com>
2018-07-17 14:14:53 +0800

    travis:update for travis ci build
    Signed-off-by: Jingru Wang <jingru@synopsys.com>

--- .travis/build.py
+++ .travis/build.py
@@ -5,47 +5,63 @@ import sys
 folder = ".travis"
 make_path = dict()
 makefile = 'makefile'
+MakefileNames = ['Makefile', 'makefile', 'GNUMakefile']
 
-def get_makefile(path): # to find the makefile in the embarc_applications
-	path = os.path.abspath(path)
+def get_makefile(path):
 	for root, dirs, files in os.walk(path):
 		dirs[:] = [d for d in dirs  if not d.startswith('.')]
 		for f in files:
-			if f == makefile or f == makefile.capitalize():
+			if f in MakefileNames:
 				key = os.path.basename(root)+'_'+os.path.basename(os.path.dirname(root))
 				make_path[key] = root
 
-def embarc_makefile(paths):
+def is_embarc_makefile(paths):
 	result = []
 	for (k,v) in paths.items():
-		with open(os.path.join(v,"Makefile")) as f:
-			embarc_root = False
-			appl = False
-			lines = f.read().splitlines()
-			for line in lines:
-				if "EMBARC_ROOT" in line:
-					embarc_root = True
-				if "APPL" in line:
-					appl = True
-			if not (embarc_root and appl):
-				result.append(k)
-			if "mwdt" in k:
-			     result.append(k)
+		for name in MakefileNames:
+			if name in os.listdir(v):
+				with open(os.path.join(v,name)) as f:
+					embarc_root = False
+					appl = False
+					lines = f.read().splitlines()
+					for line in lines:
+						if "EMBARC_ROOT" in line:
+							embarc_root = True
+						if "APPL" in line:
+							appl = True
+					if not (embarc_root and appl):
+						result.append(k)
 	for k in result:
 		paths.pop(k)
-def get_bd_ver(config):
-	bd_config = config.split(" ")[2]
-	if "11" in bd_config:
-		bd_ver = "11"
-	elif "22" in bd_config:
-		bd_ver = "22"
-	elif "23" in bd_config:
-		bd_ver = "23"
-	else:
-		bd_ver = None
-	return bd_ver
 
-def get_tcf(path):
+def get_config(config): # from input to get the config dict{"TOOLCHAIN":,"BOARD":,"BD_VER":,"CUR_CORE":}
+	make_configs = dict()
+	for make_config in config.split(" "):
+		[config_name,value] = make_config.split("=")
+		make_configs[config_name] = value
+	return make_configs
+
+def is_tcf(bd_name,bd_version,bd_core):
+	path = os.path.join(os.getcwd(),"board",bd_name,"configs",bd_version)
+	if os.path.exists(path):
+		tcf = bd_core + ".tcf"
+		if tcf in os.listdir(path):
+			return True
+		else:
+			return False
+def get_tcf(bd_name,bd_version,bd_core):
+	result = dict()
+	path = os.path.join(os.getcwd(),"board",bd_name,"configs",bd_version)
+	if os.path.exists(path):
+		tcf = bd_core + ".tcf"
+		if tcf in os.listdir(path):
+			result["name"] = tcf
+			result["path"] = os.path.join(path,tcf)
+			return result
+		else:
+			return False
+
+def get_tcfs(path):
 	result = []
 	for path in os.listdir(path):
 		suffix = os.path.splitext(path)[1]
@@ -53,37 +69,39 @@ def get_tcf(path):
 			result.append(path)
 	return result
 
-def make_file(config,bsp_config,makefile_paths):
-	bd_ver = get_bd_ver(config) #11
-	tcfs = get_tcf(os.path.join(bsp_config,bd_ver))
-	print(tcfs)
+def make_file(config):
 	result = dict()
-	for tcf in tcfs:
-		for (k,v) in makefile_paths.items():
-			if "mwdt" in k:
-				continue
-			os.chdir(os.path.join(bsp_config,bd_ver))
-			command = "tcftool " + tcf + " -q -x C_defines,arc_core_config.h -x gcc.arg"
-			print(command)
-			print(os.getcwd())
-			os.popen(command)
-			for file in os.listdir(os.getcwd()):
-				print(file)
-				
-			command = "mv arc_core_config.h gcc.arg " + v
-			os.popen(command)
-			print(command)
-			os.popen("cp ld/linker_gnu.ld " + v)
+	make_configs = get_config(config)
+	print(make_configs)
+	root = make_configs["BSP_ROOT"]
+	toolchain = make_configs["TOOLCHAIN"]
+	board = make_configs["BOARD"]
+	bd_ver = make_configs["BD_VER"]
+	cur_core = make_configs["CUR_CORE"]
+
+	print(os.getcwd())
+	if is_tcf(board,bd_ver,cur_core):
+		tcf = get_tcf(board,bd_ver,cur_core)
+
+		os.popen("tcftool " + tcf["path"] + " -q -x C_defines,arc_core_config.h -x gcc.arg")
+		tcf_dir = os.path.dirname(tcf["path"])
+		arc_core_config = os.path.join(os.getcwd(),"arc_core_config.h")
+		cacc = os.path.join(os.getcwd(),"gcc.arg")
+		linker = os.path.join(tcf_dir,"ld","linker_gnu.ld")
+
+		get_makefile(os.getcwd())
+		for (k,v) in make_path.items():
+			print("makepath...............",v)
+			os.popen("mv " + arc_core_config + " " + cacc +" " + v)
+			os.popen("cp "+ linker + " " +v)
 			os.chdir(v)
 			os.popen("mv linker_gnu.ld arc_core.ld")
 			os.system("make " + config + " clean")
-			key = tcf + "_" + k
+			key = bd_ver + "_" + cur_core+"_" +k
 			if os.system("make "+config+" -k") != 0:
 				result[key] = 1
 			else:
 				result[key] = 0
-
-
 	return result
 
 
@@ -93,11 +111,10 @@ if __name__ == '__main__':
 	# result = {}
 	cwd_path = os.getcwd() # /.travis
 	bsp_path = os.path.dirname(cwd_path) # embarc_emsk_bsp
-	bsp_application = os.path.join(bsp_path , "example")
-	bsp_config = os.path.join(bsp_path , "board/emsk/configs")
-	get_makefile(bsp_application)
-	print(make_path)
-	result = make_file(sys.argv[1],bsp_config,make_path)
+	print("input",sys.argv[1])
+
+	os.chdir(bsp_path)
+	result = make_file(sys.argv[1])
 	os.chdir(cwd_path)
 	print("Compilation result")
 	print(result)
